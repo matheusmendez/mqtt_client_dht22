e9f90ac1c434afc15da0faac090374cff3486e6c
Matheus Mendez <matheusmendez88@gmail.com>
2023-02-26 16:37:26 -0400

    modificação da coluna id

--- mqtt_client_dht22/generate_values.sql
+++ mqtt_client_dht22/generate_values.sql
@@ -1,12 +1,12 @@
 
 # CREATE DATABASE IF NOT EXISTS db_smt;
 CREATE TABLE IF NOT EXISTS db_smt.tb_temp_humi(
-	id_temp_humi INT NOT NULL AUTO_INCREMENT,
+	id INT NOT NULL AUTO_INCREMENT,
 	id_sensor varchar(20) NOT NULL,
 	temp_value FLOAT,
 	humi_value FLOAT,
 	date_time DATETIME,
-	PRIMARY KEY (id_temp_humi)
+	PRIMARY KEY (id)
 	);
 
 
