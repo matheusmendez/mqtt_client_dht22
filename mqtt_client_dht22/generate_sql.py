0322f63114730694a56da4e03c0fffd8a45d0c25
Matheus Mendez <matheusmendez88@gmail.com>
2023-02-27 10:14:29 -0400

    alteração da regra de geração de dados

--- mqtt_client_dht22/generate_sql.py
+++ mqtt_client_dht22/generate_sql.py
@@ -1,7 +1,8 @@
 import numpy as np
-from datetime import datetime
+from datetime import datetime, timedelta
 
 sensores = ['sensor_ambiente_1','sensor_ambiente_2','sensor_ambiente_3','sensor_drybox','sensor_geladeira']
+now = datetime.now()
 
 sql="""
 # CREATE DATABASE IF NOT EXISTS db_smt;
@@ -20,7 +21,7 @@ for i in range(10000):
     id_sensor = np.random.choice(sensores)
     temp = np.random.uniform(low=18.0, high=30.0, size=None)
     humi = np.random.uniform(low=20.0, high=60.0, size=None)
-    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    date = (now + timedelta(minutes=i+5)).strftime("%Y%m%d%H%M%S")
     sql += f'\nINSERT INTO db_smt.tb_temp_humi (id_sensor,temp_value,humi_value,date_time) VALUES ("{id_sensor}",{temp:.2f},{humi:.2f},{date});'
 
 with open(file='generate_values.sql',mode='w') as f:
